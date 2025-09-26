ee06be88c4b0a43ec08c17504c38b0fd29bdb191
sher913 <shermanchan@rocketmail.com>
2025-09-26 17:14:55 +0800

    Scaffold: Next.js SaaS Starter

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    ppr: true,
+    clientSegmentCache: true,
+    nodeMiddleware: true
+  }
 };
 
 export default nextConfig;
